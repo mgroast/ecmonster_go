76d2237e627d967f184c7e0588e4ead42d12bd43
mgroast <mgroast@gmail.com>
2019-08-24 23:40:34 +0900

    indexのルーティング

---
+++ controllers/index.go
@@ -0,0 +1,10 @@
+package controllers
+
+import(
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func GetIndex(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", gin.H{})
+}
--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
 import(
-    "github.com/gin-gonic/gin"
-    c "github.com/mgroast/ecmonster/controllers"
+	"github.com/gin-gonic/gin"
+	c "github.com/mgroast/ecmonster/controllers"
 )
 
 func main() {
-    r := gin.Default()
-    r.LoadHTMLGlob("views/*")
-    r.GET("/", c.GetUsers)
-    r.Run(":8080")
+	r := gin.Default()
+	r.LoadHTMLGlob("views/*")
+	r.GET("/", c.GetIndex)
+	r.Run(":8080")
 }
